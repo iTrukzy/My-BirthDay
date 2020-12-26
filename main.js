774c557f872a0a3b81c6e26162db0c4e3ebd5c58
william herrera <wjha.1998@hotmail.com>
2020-12-26 11:00:39 -0500

    happy birthday

---
+++ main.js
@@ -0,0 +1,42 @@
+const daysEl = document.getElementById('days');
+const hoursEl = document.getElementById('hours');
+const minsEl = document.getElementById('mins');
+const secondsEl = document.getElementById('seconds');
+
+
+
+const newYears = '11 May 2021';
+
+
+
+
+function countDown() {
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date();
+
+    const totalSeconds = (newYearsDate - currentDate) / 1000;
+
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const minutes = Math.floor(totalSeconds / 60) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = formateTime(hours);
+    minsEl.innerHTML = formateTime(minutes);
+    secondsEl.innerHTML = formateTime(seconds);
+}
+
+
+
+function formateTime (time) {
+    return time < 10 ? `0${time}` : time;
+}
+
+
+countDown();
+
+
+setInterval(countDown, 1000)
